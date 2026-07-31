457371cb963d280607af57a757f8c849f3f4e4e2
Harsh Patidar <harshptidar1234@gmail.com>
2026-08-01 00:05:01 +0530

    Added solution - Time: O(n^2), Space: O(1)

--- 700148-insertion-sort-GFG/700148-insertion-sort.py
+++ 700148-insertion-sort-GFG/700148-insertion-sort.py
@@ -3,14 +3,13 @@ class Solution:
         # code here
         n = len(arr)
         for i in range(1,n):
-            if(arr[i]<arr[i-1]):
-                key = arr[i]
-                for j in range(i-1,-2,-1):
-                    if j<0 or (arr[j]<key) :
-                        arr[j+1] = key
-                        break
-                    else :
-                        arr[j+1] = arr[j]
+            key = arr[i]
+            j = i-1
+            while j>=0 and (key<arr[j]):  
+                arr[j+1] = arr[j]
+                j-=1
+            
+            arr[j+1] = key
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
