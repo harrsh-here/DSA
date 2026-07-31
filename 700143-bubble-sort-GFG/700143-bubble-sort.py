c629a5405524729fbed4e2f1dbac2ebc8675a576
Harsh Patidar <harshptidar1234@gmail.com>
2026-07-31 19:58:49 +0530

    Added solution - Time: O(n^2), Space: O(1)

--- 700143-bubble-sort-GFG/700143-bubble-sort.py
+++ 700143-bubble-sort-GFG/700143-bubble-sort.py
@@ -3,9 +3,13 @@ class Solution:
         # code here
         n = len(arr)
         for i in range(n-2,-1,-1):
+            is_swap = False
             for j in range(i+1):
                 if arr[j]>arr[j+1]:
                     arr[j], arr[j+1] = arr[j+1], arr[j]
+                    is_swap = True
+            if is_swap==False:
+                break
     
 
 # Synced seamlessly with LeetHub Pro
