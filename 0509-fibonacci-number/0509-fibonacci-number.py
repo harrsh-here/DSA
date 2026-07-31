be65f00eec9515318cd5734abd9cd3be47fa09e4
Harsh Patidar <harshptidar1234@gmail.com>
2026-07-31 23:40:10 +0530

    Time: 16 ms (66.96%), Space: 12.3 MB (53.42%) - LeetHub - Time: O(n), Space: O(n)

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,18 @@
-class Solution(object):
-    def fib(self, num):     
-        if num == 0 or num == 1 :
-            return num
-        return self.fib(num-1) + self.fib(num-2)
-    
-        
+class Solution:
+    def fib(self, n):
+        memo = {}
+
+        def solve(x):
+            if x in memo:
+                return memo[x]
+
+            if x <= 1:
+                return x
+
+            memo[x] = solve(x-1) + solve(x-2)
+            return memo[x]
+
+        return solve(n)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
