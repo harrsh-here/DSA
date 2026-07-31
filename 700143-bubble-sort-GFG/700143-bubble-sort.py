76a442094eca9f8811953ee79c5336ddecf27d46
Harsh Patidar <harshptidar1234@gmail.com>
2026-07-31 19:44:08 +0530

    Added solution - Time: O(n^2), Space: O(1)

---
+++ 700143-bubble-sort-GFG/700143-bubble-sort.py
@@ -0,0 +1,13 @@
+class Solution:
+    def bubbleSort(self,arr):
+        # code here
+        n = len(arr)
+        for i in range(n-2,-1,-1):
+            for j in range(i+1):
+                if arr[j]>arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+    
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
