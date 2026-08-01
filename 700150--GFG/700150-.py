0fe1f3de7a1ebf87d497aec24ea2d26657ea6905
Harsh Patidar <harshptidar1234@gmail.com>
2026-08-01 22:36:50 +0530

    Added solution - Time: O(n log n), Space: O(n)

--- 700150--GFG/700150-.py
+++ 700150--GFG/700150-.py
@@ -1,45 +1,45 @@
 class Solution:
     def mergeSort(self, arr, l, r):
         # code here
-        if len(arr)<=1:
-            return arr
-        n = len(arr)
+        if l>=r:
+            return
+        n = l+r
+        
         mid = n//2
-        left_arr = arr[:mid]
-        right_arr = arr[mid:]
-        
-        left = self.mergeSort(left_arr,l,mid)
-        right = self.mergeSort(right_arr,mid,r)
-        sorted_arr = self.merge(left, right)
 
-        for i in range(len(sorted_arr)):
-            arr[i] = sorted_arr[i]
         
+        self.mergeSort(arr,l,mid)
+        self.mergeSort(arr,mid+1,r)
+        self.merge(arr, l,mid,r)
         return arr
         
         
-    def merge(self,left,right):
-        result = []
-        i,j = 0,0
+    def merge(self,arr,l,mid,r):
+        left = arr[l:mid+1]
+        right = arr[mid+1:r+1]
+        i,j,k = 0,0,l
         n,m = len(left), len(right)
         
         while i<n and j<m:
             if left[i]<=right[j]:
-                result.append(left[i])
+                arr[k] = left[i]
                 i+=1
+                k+=1
             else : 
-                result.append(right[j])
+                arr[k] = right[j]
                 j+=1
-        if i<n:
-            while i<n :
-                result.append(left[i])
-                i+=1
-        if j<m:
-            while j<m:
-                result.append(right[j])
-                j+=1
-        return result
+                k+=1
         
+        while i<n :
+            arr[k] = left[i]
+            i+=1
+            k+=1
+        
+        while j<m:
+            arr[k] = right[j]
+            j+=1
+            k+=1
+        return 
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
