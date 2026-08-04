c93c4ac4124349f0d3947f69cdc228b6cc23f4f3
Harsh Patidar <harshptidar1234@gmail.com>
2026-08-04 21:28:51 +0530

    Added solution - Time: O(sqrt(n)), Space: O(1)

--- 703889-number-of-factors-GFG/703889-number-of-factors.py
+++ 703889-number-of-factors-GFG/703889-number-of-factors.py
@@ -1,12 +1,16 @@
 class Solution:
     def countFactors (self, n):
         # code here
+        num =n
         count = 0
-        for i in range(1,n//2+1):
-            if n%i == 0:
+        for i in range(1, int((num**0.5)+1)):
+            
+            if num%i == 0:
+                count+=1
                 
-                count += 1
-        count+=1
+                if num//i != i:
+                    count+=1
+                    
         return count
 
 # Synced seamlessly with LeetHub Pro
