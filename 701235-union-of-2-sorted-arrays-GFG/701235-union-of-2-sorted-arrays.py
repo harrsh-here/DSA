943ead07e5e52338e3b39622b7339ecf9aaa2c07
Harsh Patidar <harshptidar1234@gmail.com>
2026-08-11 14:56:01 +0530

    Added solution - Time: O(n + m), Space: O(n + m)

--- 701235-union-of-2-sorted-arrays-GFG/701235-union-of-2-sorted-arrays.py
+++ 701235-union-of-2-sorted-arrays-GFG/701235-union-of-2-sorted-arrays.py
@@ -5,29 +5,29 @@ class Solution:
         m = len(b)
         new = []
         i,j = 0,0
-        last_element = None
+        
         while i<n and j<m:
             if a[i] <= b[j]:
-                if last_element != a[i]:
+                if len(new)==0 or new[-1] != a[i]:
                     new.append(a[i])
-                    last_element = a[i]
+                    
                 i+=1
             else:
-                if last_element != b[j]:
+                if len(new)==0 or new[-1] != b[j]:
                     new.append(b[j])
-                    last_element = b[j]
+                    
                 j+=1
             
             
         while i<n:
-            if last_element != a[i]:
+            if len(new)==0 or new[-1] != a[i]:
                 new.append(a[i])
-                last_element = a[i]
+               
             i+=1
         while j<m:
-            if last_element != b[j]:
+            if len(new)==0 or new[-1] != b[j]:
                 new.append(b[j])
-                last_element = b[j]
+                
             j+=1
         return new
 
