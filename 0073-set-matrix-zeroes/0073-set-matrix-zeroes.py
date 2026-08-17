7dda800dc868d6e970c252fd41c2be2a69c210d6
Harsh Patidar <harshptidar1234@gmail.com>
2026-08-17 07:56:41 +0530

    Optimal Time: 9 ms (73.85%), Space: 13.9 MB (13.5%) - LeetHub - Time: O(r*c), Space: O(r+c)

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,23 +1,20 @@
 class Solution(object):
     def setZeroes(self, matrix):
+       
         r = len(matrix)
         c = len(matrix[0])
+        row_track = [0]*r
+        col_track = [0]*c
 
         for i in range(r):
             for j in range(c):
                 if matrix[i][j] == 0:
-                    
-                    for k in range(c):
-                        if matrix[i][k]:
-                            matrix[i][k]= float('inf')
-                    for k in range(r):
-                        if matrix[k][j]:
-                            matrix[k][j] = float('inf') 
+                    row_track[i]=-1
+                    col_track[j] = -1
         for i in range(r):
             for j in range(c):
-                if matrix[i][j] == float('inf'):
-                    matrix[i][j] = 0 
-        
+                if row_track[i] == -1 or col_track[j] == -1:
+                    matrix[i][j] = 0
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
