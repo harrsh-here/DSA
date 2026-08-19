7725342ff260906d9eb25dfc97d45ab888d7e39e
Harsh Patidar <harshptidar1234@gmail.com>
2026-08-19 23:01:40 +0530

    Optimal Time: 0 ms (100%), Space: 12.5 MB (1.64%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,22 +4,15 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        
         r = len(matrix)
         c = len(matrix[0])
 
-        result = [[0]*c for _ in range(r)]
-        
         for i in range(r):
-            for j in range(c):
-                # print(f"{j},{c - i - 1} : ", result[j][c - i - 1])
-                result[j][c-i-1] = matrix[i][j]
-                # print(f"{j},{c-i-1} : ",result[j][c-i-1])
-
-        # for i in range(r):
-        #     for j in range(c):
-                # arr[i][j] = (
-                # print(f"{j},{c-i-1} : ",result[j][c-i-1])
-        matrix[:] = result
+            for j in range(i+1,c):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(r):
+            matrix[i].reverse()
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
