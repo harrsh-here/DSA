2b9183e35b539c716de46a4ab66c1329c4d655e8
Harsh Patidar <harshptidar1234@gmail.com>
2026-09-06 09:20:34 +0530

    Time: 1 ms (32.77%), Space: 15.7 MB (44.16%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not head :
+            return head
+        curr_odd = head
+        even_head= curr_odd.next
+        curr_even = even_head
+        while curr_odd.next and curr_even.next:
+            curr_odd.next = curr_even.next
+            curr_odd = curr_even.next
+
+            curr_even.next = curr_odd.next
+            curr_even = curr_odd.next
+        curr_odd.next = even_head
+        
+        return head
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
