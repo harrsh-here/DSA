74844404dcd1c1d7cbd91aba750cd054e7344756
Harsh Patidar <harshptidar1234@gmail.com>
2026-09-06 09:24:21 +0530

    Time: 0 ms (100%), Space: 15.9 MB (9.87%) - LeetHub - Time: O(n), Space: O(1)

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -4,20 +4,21 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        if not head :
+        if not head:
             return head
         curr_odd = head
-        even_head= curr_odd.next
+        even_head = curr_odd.next
         curr_even = even_head
         while curr_odd.next and curr_even.next:
             curr_odd.next = curr_even.next
             curr_odd = curr_even.next
 
             curr_even.next = curr_odd.next
-            curr_even = curr_odd.next
+            curr_even = curr_even.next
         curr_odd.next = even_head
-        
+
         return head
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
