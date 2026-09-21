b7cc1a3339cae4875daabe36e51a105207f32812
Harsh Patidar <harshptidar1234@gmail.com>
2026-09-21 11:18:44 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 710059-pair-sum-in-sorted-doubly-linked-list-GFG/710059-pair-sum-in-sorted-doubly-linked-list.py
@@ -0,0 +1,34 @@
+# Structure of Doubly Linked List Node
+'''
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+        self.prev = None
+'''
+
+class Solution:
+    def givenSumPairs(self, head, target):
+        # code here
+        res =  []
+        curr = head
+        while curr.next:
+            curr = curr.next
+        p = head
+        q = curr
+        
+        while p.data<q.data:
+            total = p.data +q.data
+            if total == target:
+                res.append([p.data,q.data])
+                p = p.next
+                q = q.prev
+            if total < target:
+                p = p.next
+            if total > target:
+                q = q.prev
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
